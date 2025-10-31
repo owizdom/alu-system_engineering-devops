232c777fce7f127154005dfacce6c5fa2803f3ff
owizdom <gokun4621@gmail.com>
2025-10-31 14:10:00 +0000

    newnew

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,16 @@
-Host *
-	PasswordAuthentication no
-	IdentityFile ~/.ssh/school
+# Configures ssh for dev
+include stdlib
+
+file_line { 'no_password_auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'identity_file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
+}
